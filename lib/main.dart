5c93d21edf4c9d1216dd1019473dfc928e0eaf3b
yo.ver <yvr.collantes@gmail.com>
2025-08-12 00:07:25 -0500

    hola mundo

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const Text("Hola mundo"), //const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
